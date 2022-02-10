b077140a4efdcb252746b53e49bb3932727048aa
konrad <konradcz02@gmail.com>
2022-02-10 17:36:31 +0100

    created header with theme switch

---
+++ src/components/organisms/Header/Header.styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+
+  h1 {
+    margin: 0;
+  }
+`;
